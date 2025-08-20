61a522f52155725c8e7e7899ee32b1a85fdf42db
darsh <fatwanidarsh1@gmail.com>
2025-08-20 22:07:59 +0530

    Started implementing JWTSERVICE

--- app/src/main/java/org/example/service/CustomUserDetails.java
+++ app/src/main/java/org/example/service/CustomUserDetails.java
@@ -12,6 +12,27 @@ import java.util.List;
 
 //UserDetails has methods that needs to be implemented and overriden
 //Spring Security’s login mechanism always works with UserDetails, not your custom UserInfo entity directly.
+/*Spring Security is a framework that handles authentication (who are you?) and authorization (what can you do?).
+When a user logs into your app:
+They enter a username and password.
+Spring Security needs to fetch that user from the database.
+It needs to know their roles/permissions.
+But Spring Security doesn’t know about your custom UserInfo entity. It only understands a very specific format: UserDetails.
+So we must adapt our entity into a format that Spring Security understands. That’s where CustomUserDetails comes in.*/
+/*iSimilarly, Spring Security defines an interface:
+public interface UserDetails {
+    String getUsername();
+    String getPassword();
+    Collection<? extends GrantedAuthority> getAuthorities();
+    boolean isAccountNonExpired();
+    boolean isAccountNonLocked();
+    boolean isCredentialsNonExpired();
+    boolean isEnabled();
+}
+
+This is the contract.
+If we make a class that implements UserDetails, then Spring can talk to it.*/
+
 public class CustomUserDetails extends UserInfo
         implements UserDetails
 {
@@ -19,6 +40,10 @@ public class CustomUserDetails extends UserInfo
     private String username;
     private String password;
     Collection<? extends GrantedAuthority> authorities;
+/*GrantedAuthority is an interface, not an enum. It represents a single authority, like a role or a permission. Spring provides a simple implementation called SimpleGrantedAuthority, which just wraps a string such as ROLE_ADMIN or READ_REPORTS.
+The actual role names are not predefined in Spring Security — we define them ourselves, either in the database or in configuration.
+The only convention is that roles usually start with ROLE_
+ */
 
     public CustomUserDetails(UserInfo byUsername) {
         this.username = byUsername.getUsername();
