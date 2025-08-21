67de4f64e6c9d9a2205fca114ad9077fcf0d98d2
darsh <fatwanidarsh1@gmail.com>
2025-08-21 14:11:03 +0530

    Repositorypackage

---
+++ app/src/main/java/org/example/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package org.example.repository;
+
+import org.aspectj.apache.bcel.Repository;
+import org.example.entities.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Integer> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
